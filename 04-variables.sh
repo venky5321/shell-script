c91e6ef240d6a9b1bd035a163b60ced32dcf2716
venky5321 <venkymadumanchi.com>
2026-07-28 10:19:56 +0530

    hello script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you? "
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am good. How is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
