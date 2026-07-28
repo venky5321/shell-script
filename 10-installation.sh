6dfcf9a78854e690f6bbbf070902217743be652e
venky5321 <venkymadumanchi.com>
2026-07-28 12:41:53 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -12,4 +12,21 @@ fi
 
 dnf install mysql -y
 
+if [$? -ne 0]
+then
+    echo "Installation of mysql...FAILURE"
+    exit 1
+else
+    echo "Installation of mysql...SUCCESS"    
+fi  
+
+dnf install git -y 
+
+if [$? -ne 0]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of git...SUCCESS"    
+fi  
 echo "is script proceeding?"
