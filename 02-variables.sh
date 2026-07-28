4ee519e7c43330282d03ddbe6acf0e3e0c434197
venky5321 <venkymadumanchi.com>
2026-07-28 09:44:53 +0530

    hello script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is again sample script
+#!/bin/bash
+
+echo "Venky:: Hello Surya, How are you? "
+echo "Surya:: Hi Venky, I am fine. How are you?"
+echo "Venky:: I am good. How is your work?"
+echo "Surya:: not bad. I am thinking to upgrade to DevOps"
