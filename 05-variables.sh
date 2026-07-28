d46483916e34a0a95767bcb606749bb2793203c0
venky5321 <venkymadumanchi.com>
2026-07-28 10:29:45 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME  # here USERNAME is variables
+
+echo "Please enter password::"
+
+read PASSWORD
+
+echo "USERNAME is: $USERNAME, PASSWORD is: $PASSWORD"
