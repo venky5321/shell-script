1283faa9f9ae4492fe88133727c87deb93f7d7b4
venky5321 <venkymadumanchi.com>
2026-07-29 09:12:17 +0530

    function

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
  
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | CUT -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -21,10 +24,10 @@ else
     echo "you are super user."    
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 
 VALIDATE $? "Installing MYSQL"
 
-dnf install git -y 
+dnf install git -y &>>$LOGFILE
 
 VALIDATE $? "Installing Git"
