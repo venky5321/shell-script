2a930448e5d724e7e61db9483e07ffa47b1016d5
venky5321 <venkymadumanchi.com>
2026-07-30 10:29:25 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -3,6 +3,10 @@
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -30,7 +34,7 @@ do
    dnf list installed $i &>>$LOGFILE
    if [ $? -eq 0 ]
    then
-       echo "$i already installed...SKIPPING"
+       echo -e "$i already installed...$Y SKIPPING $N"
     else
        echo "$i not installed...Need to install"   
     fi   
