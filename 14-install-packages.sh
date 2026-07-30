6a8fe912fefb0ba5476634d5ca67c99c1ba891f0
venky5321 <venkymadumanchi.com>
2026-07-30 10:24:51 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+       echo -e "$2...$R FAILURE $N"
+       exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"   
+    fi   
+
+}
+
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
@@ -12,5 +27,11 @@ fi
 for i in $@
 do
    echo "All packages: $i"
-   dnf list installed $i
+   dnf list installed $i &>>$LOGFILE
+   if [ $? -eq 0 ]
+   then
+       echo "$i already installed...SKIPPING"
+    else
+       echo "$i not installed...Need to install"   
+    fi   
 done
