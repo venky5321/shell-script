25424279765406ec7ebdd336f7171113b36721b5
venky5321 <venkymadumanchi@gmail.com>
2026-08-04 11:20:16 +0530

    functions

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,8 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-others-script.sh
+#./16-others-script.sh
+
+Source./16-others-script.sh
 
 echo "After calling other script, course: $COURSE"
