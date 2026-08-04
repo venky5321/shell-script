cdd7b81451bcc783bce33568f72e76f5a4bb165f
venky5321 <venkymadumanchi@gmail.com>
2026-08-04 15:31:40 +0530

    functions

---
+++ expense-shell-01/frontend.sh
@@ -0,0 +1,25 @@
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting Nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing existing content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend code"
+
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expense conf"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting Nginx"
